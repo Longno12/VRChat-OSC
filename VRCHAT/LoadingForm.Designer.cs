64afb29e6ade37cdbd697f0f1872712903bc02b4
Longno12 <157077978+Longno12@users.noreply.github.com>
2025-10-28 20:22:49 +0000

    Update LoadingForm.Designer.cs

--- VRCHAT/LoadingForm.Designer.cs
+++ VRCHAT/LoadingForm.Designer.cs
@@ -12,9 +12,7 @@
             this.lblStatus = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
+
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
@@ -23,9 +21,7 @@
             this.label1.Size = new System.Drawing.Size(296, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "VRChat OSC Pro C#";
-            // 
-            // lblStatus
-            // 
+
             this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblStatus.ForeColor = System.Drawing.Color.Gainsboro;
             this.lblStatus.Location = new System.Drawing.Point(12, 118);
@@ -34,18 +30,14 @@
             this.lblStatus.TabIndex = 1;
             this.lblStatus.Text = "Initializing...";
             this.lblStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // progressBar1
-            // 
+
             this.progressBar1.Location = new System.Drawing.Point(78, 86);
             this.progressBar1.MarqueeAnimationSpeed = 50;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(245, 10);
             this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
             this.progressBar1.TabIndex = 2;
-            // 
-            // LoadingForm
-            // 
+
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
@@ -66,4 +58,5 @@
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.ProgressBar progressBar1;
     }
+
 }
